f638e4de5aa9a8def7418d215c54633f2771ecb0
David Foucher <dev@tyjak.net>
2015-05-04 10:59:16 +0200

    only keep nginx-phpfpm

--- baseimage-apache/composer/setup.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-# install composer
-curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
--- mailarchiva/run-mailarchiva.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-exec /etc/init.d/mailarchiva start >>/var/log/mailarchiva.log 2>&1
